e31fd1ae4487043b88e0015b517b83b8e71c8b7f
Jonas Lit <melynasdangus@gmail.com>
2022-07-19 00:29:52 +0300

    Added playRound() function
    This function takes in two args - a player choice and a computer choice and returns who is the winner of the round.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-console.log(getComputerChoice());
-
 function getComputerChoice() {
     let num = Math.floor(Math.random()*3);
     switch(num){
@@ -12,5 +10,30 @@ function getComputerChoice() {
         case 2:
             return "Scissors";
             break;
+    }
+}
+
+function playRound(playerChoice, computerChoice) {
+
+    playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1).toLowerCase();
+
+    if (playerChoice === 'Rock' && computerChoice === 'Paper') {
+        return `You lose, paper beats rock!`;
+    } else if (playerChoice === 'Rock' && computerChoice === 'Rock') {
+        return `It's a tie.`;
+    } else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
+        return `You win. Rock beats scissors.`;
+    } else if (playerChoice === 'Paper' && computerChoice === 'Rock') {
+        return `You win, paper beats rock!`;
+    } else if (playerChoice === 'Paper' && computerChoice === 'Paper') {
+        return `It's a tie.`;
+    } else if (playerChoice === 'Paper' && computerChoice === 'Scissors') {
+        return `You lose. scissors beat paper.`;
+    } else if (playerChoice === 'Scissors' && computerChoice === 'Rock') {
+        return `You lose. Rock beats scissors.`;
+    } else if (playerChoice === 'Scissors' && computerChoice === 'Paper') {
+        return `You win. Scissors beat Paper.`;
+    } else if (playerChoice === 'Scissors' && computerChoice === 'Scissors') {
+        return `It's a tie.`
     }
 }
