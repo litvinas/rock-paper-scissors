ff3705827335643949726b2aee010f69383319ea
Jonas Lit <melynasdangus@gmail.com>
2022-07-19 00:08:17 +0300

    Add function getComputerChoice
    the function randomly returns either 'Rock', 'Paper' or 'Scissors'.'

---
+++ javascript.js
@@ -0,0 +1,16 @@
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random()*3);
+    switch(num){
+        case 0: 
+            return "Rock"; 
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+    }
+}
